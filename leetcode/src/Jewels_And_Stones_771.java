0debcb7136df39d31d8ebe211b3700e3492b611b
Alexey Studenikin <alexey.studenikin@adv-cake.ru>
2019-05-29 15:34:26 +0300

    771

---
+++ leetcode/src/Jewels_And_Stones_771.java
@@ -0,0 +1,14 @@
+public class Jewels_And_Stones_771 {
+    public int numJewelsInStones(String J, String S) {
+        int hits = 0;
+
+        for (char ch : J.toCharArray()) {
+            for (int i = 0; i < S.length(); ++i) {
+                if (ch == S.charAt(i)) ++hits;
+            }
+        }
+        return hits;
+    }
+}
+
+// https://leetcode.com/problems/jewels-and-stones/
