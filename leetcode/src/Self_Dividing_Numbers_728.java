7c40060478076f3777dfe5d595be3b3f46016b62
Alexey Studenikin <avstdn@gmail.com>
2019-05-29 13:41:07 +0300

    init

---
+++ leetcode/src/Self_Dividing_Numbers_728.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Self_Dividing_Numbers_728 {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> list = new ArrayList<>();
+        while (left <= right) {
+            if (isDivide(left)) list.add(left);
+            left++;
+        }
+        return list;
+    }
+
+    public boolean isDivide(int num) {
+        int remainder = num;
+        while (remainder > 0) {
+            if (remainder % 10 == 0 || num % (remainder % 10) != 0) return false;
+            remainder /= 10;
+        }
+        return true;
+    }
+}
+
+// https://leetcode.com/problems/self-dividing-numbers/
