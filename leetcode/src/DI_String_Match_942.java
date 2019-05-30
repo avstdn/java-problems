7dd3f2fcf52950af7c260af88f3e78b26771df29
Alexey Studenikin <alexey.studenikin@adv-cake.ru>
2019-05-30 09:27:05 +0300

    13 new problems

---
+++ leetcode/src/DI_String_Match_942.java
@@ -0,0 +1,22 @@
+public class DI_String_Match_942 {
+    public int[] diStringMatch(String S) {
+        int inc = 0;
+        int dec = S.length();
+        int[] result = new int[S.length() + 1];
+
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == 'I') {
+                result[i] = inc;
+                inc++;
+                if (i == S.length() - 1) result[i+1] = inc;
+            } else {
+                result[i] = dec;
+                dec--;
+                if (i == S.length() - 1) result[i+1] = dec;
+            }
+        }
+
+        return result;
+    }
+}
+// https://leetcode.com/problems/di-string-match/
