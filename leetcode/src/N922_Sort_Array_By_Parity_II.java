f2250df8a9b4a8042ec3f6ec8e77cb6a7eb6e613
Alexey Studenikin <avstdn@gmail.com>
2019-05-31 18:50:08 +0300

    922

---
+++ leetcode/src/N922_Sort_Array_By_Parity_II.java
@@ -0,0 +1,19 @@
+public class N922_Sort_Array_By_Parity_II {
+    public int[] sortArrayByParityII(int[] A) {
+        int[] result = new int[A.length];
+        int even = 0;
+        int odd = 1;
+
+        for (int i : A) {
+            if (i % 2 == 0) {
+                result[even] = i;
+                even += 2;
+            } else {
+                result[odd] = i;
+                odd += 2;
+            }
+        }
+        return result;
+    }
+}
+// https://leetcode.com/problems/sort-array-by-parity-ii/submissions/
