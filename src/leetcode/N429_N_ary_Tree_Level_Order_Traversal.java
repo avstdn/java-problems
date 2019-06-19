08c72ef1f14fd23a92e458048d244fdd4247bd8b
Alexey Studenikin <avstdn@gmail.com>
2019-06-19 23:53:41 +0300

    Update 429

--- src/leetcode/N429_N_ary_Tree_Level_Order_Traversal.java
+++ src/leetcode/N429_N_ary_Tree_Level_Order_Traversal.java
@@ -19,6 +19,19 @@ import java.util.List;
  *      [3,2,4],
  *      [5,6]
  * ]
+ *
+ * Definition for a Node.
+ * class Node {
+ *     public int val;
+ *     public List<Node> children;
+ *
+ *     public Node() {}
+ *
+ *     public Node(int _val, List<Node> _children) {
+ *         val = _val;
+ *         children = _children;
+ *     }
+ * };
  */
 public class N429_N_ary_Tree_Level_Order_Traversal {
     public List<List<Integer>> levelOrder(Node root) {
