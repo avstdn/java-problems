d38410919dfd32efde93c98ef1ba255b05cd8a64
Alexey Studenikin <avstdn@gmail.com>
2019-06-21 00:27:04 +0300

    509 and 476 edit

--- src/leetcode/N509_Fibonacci_Number.java
+++ src/leetcode/N509_Fibonacci_Number.java
@@ -1,10 +1,10 @@
 package leetcode;
 
 public class N509_Fibonacci_Number {
-     public int fibRecursive(int N) {
-         if (N < 2) return N;
-         return fibRecursive(N - 1) + fibRecursive(N - 2);
-     }
+    public int fibRecursive(int N) {
+        if (N < 2) return N;
+        return fibRecursive(N - 1) + fibRecursive(N - 2);
+    }
 
     public int fibIterative(int N) {
         if (N < 2) return N;
@@ -24,6 +24,18 @@ public class N509_Fibonacci_Number {
             last = fib - last;
         }
         return fib;
+    }
+
+    public int fibClassic(int N) {
+        int a = 1;
+        int b = 1;
+
+        for (int i = 2; i < N; i++) {
+            a += b;
+            b = a - b;
+        }
+
+        return N > 0 ? a : 0;
     }
 }
 // https://leetcode.com/problems/fibonacci-number/submissions/
