b34604d8a44b98a8981313155992b4e93b2589ef
Alexey Studenikin <avstdn@gmail.com>
2019-08-28 23:51:50 +0300

    387

--- src/leetcode/N387_First_Unique_Character_in_a_String.java
+++ src/leetcode/N387_First_Unique_Character_in_a_String.java
@@ -3,6 +3,21 @@ package leetcode;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Given a string, find the first non-repeating character in it and return it's index.
+ * If it doesn't exist, return -1.
+ *
+ * Example 1:
+ * s = "leetcode"
+ * return 0.
+ *
+ * Example 2:
+ * s = "loveleetcode",
+ * return 2.
+ *
+ *  Note: You may assume the string contain only lowercase letters.
+ */
+
 public class N387_First_Unique_Character_in_a_String {
     public int firstUniqChar(String s) {
         Map<Character, Integer> charsMap = new HashMap<>();
